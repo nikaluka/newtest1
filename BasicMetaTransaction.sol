d27a2f18663029d6526803113c3f42f0863b4016
nikaluka <47041738+nikaluka@users.noreply.github.com>
2021-07-11 16:33:22 +0200

    Create BasicMetaTransaction.sol

---
+++ BasicMetaTransaction.sol
@@ -0,0 +1,100 @@
+pragma solidity 0.6.0;
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+        return c;
+    }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+contract BasicMetaTransaction {
+    using SafeMath for uint256;
+    event MetaTransactionExecuted(address userAddress, address payable relayerAddress, bytes functionSignature);
+    mapping(address => uint256) private nonces;
+    function getChainID() public pure returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+    function executeMetaTransaction(address userAddress, bytes memory functionSignature,
+        bytes32 sigR, bytes32 sigS, uint8 sigV) public payable returns(bytes memory) {
+        require(verify(userAddress, nonces[userAddress], getChainID(), functionSignature, sigR, sigS, sigV), "Signer and signature do not match");
+        nonces[userAddress] = nonces[userAddress].add(1);
+        // Append userAddress at the end to extract it from calling context
+        (bool success, bytes memory returnData) = address(this).call(abi.encodePacked(functionSignature, userAddress));
+        require(success, "Function call not successful");
+        emit MetaTransactionExecuted(userAddress,msg.sender, functionSignature);
+        return returnData;
+    }
+    function getNonce(address user) external view returns(uint256 nonce) {
+        nonce = nonces[user];
+    }
+
+    // Builds a prefixed hash to mimic the behavior of eth_sign.
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+
+    function verify(address owner, uint256 nonce, uint256 chainID, bytes memory functionSignature,
+        bytes32 sigR, bytes32 sigS, uint8 sigV) public view returns (bool) {
+
+        bytes32 hash = prefixed(keccak256(abi.encodePacked(nonce, this, chainID, functionSignature)));
+        address signer = ecrecover(hash, sigV, sigR, sigS);
+        require(signer != address(0), "Invalid signature");
+		return (owner == signer);
+    }
+
+    function msgSender() internal view returns(address sender) {
+        if(msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else {
+            return msg.sender;
+        }
+    }
+}
